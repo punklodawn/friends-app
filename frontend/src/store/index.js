1a2620f798fec030167b509226105fa97bf295ee
Yilmer Avila <yilmer0001@hotmail.com>
2021-03-29 12:31:53 -0500

    Disable inline runtime chunck on front build

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,14 +1,16 @@
-import { createStore, compose } from 'redux';
+// import { createStore, compose } from 'redux';
+import { createStore } from 'redux';
 
 import rootReducer from "../reducers";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ trace: true, traceLimit: 25 }) || compose;
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ trace: true, traceLimit: 25 }) || compose;
 
 // const store = createStore(rootReducer,applyMiddleware(thunk));
 // const store = createStore(rootReducer, composeEnhancers(
 //     applyMiddleware(thunk)
 // ));
-const store = createStore(rootReducer, composeEnhancers());
+// const store = createStore(rootReducer, composeEnhancers());
+const store = createStore(rootReducer);
 
 export default store;
 
